e0369becdee04ff070d67d49d3b5978914ae83aa
abhishekmishravlog <abhishek.mish2002@gmail.com>
2023-03-24 10:13:00 +0530

    Welcome and Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Grid, makeStyles } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Welcome, { ErrorPage } from "./component/Welcome";
+import Navbar from "./component/Navbar";
+
 
 export default App;
