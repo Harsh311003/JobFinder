46da72422d597638ab89894400e9542ac240f2fa
abhishekmishravlog <abhishek.mish2002@gmail.com>
2023-03-25 13:46:49 +0530

    Home and Navbar added

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,95 +1,24 @@
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-    Button,
-    makeStyles,
-  } from "@material-ui/core";
-  import { useHistory } from "react-router-dom";
-  
-  import isAuth, { userType } from "../lib/isAuth";
-  
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-  
-  const Navbar = (props) => {
-    const classes = useStyles();
-    let history = useHistory();
-  
-    const handleClick = (location) => {
-      console.log(location);
-      history.push(location);
-    };
-  
-    return (
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Job Portal
-          </Typography>
-          {isAuth() ? (
-            userType() === "recruiter" ? (
-              <>
-                <Button color="inherit" onClick={() => handleClick("/home")}>
-                  Home
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/addjob")}>
-                  Add Jobs
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/myjobs")}>
-                  My Jobs
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/employees")}>
-                  Employees
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/profile")}>
-                  Profile
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/logout")}>
-                  Logout
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button color="inherit" onClick={() => handleClick("/home")}>
-                  Home
-                </Button>
-                <Button
-                  color="inherit"
-                  onClick={() => handleClick("/applications")}
-                >
-                  Applications
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/profile")}>
-                  Profile
-                </Button>
-                <Button color="inherit" onClick={() => handleClick("/logout")}>
-                  Logout
-                </Button>
-              </>
-            )
-          ) : (
-            <>
-              <Button color="inherit" onClick={() => handleClick("/login")}>
-                Login
-              </Button>
-              <Button color="inherit" onClick={() => handleClick("/signup")}>
-                Signup
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-    );
-  };
-  
-  export default Navbar;
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+      <div className="nav-left">
+        <a href="/"><h1>Job Finder</h1></a>
+      </div>
+      <div className="nav-right">
+        <div class="dropdown cls">
+          <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">SIGNUP AS
+            <span class="caret"></span></button>
+          <ul class="dropdown-menu">
+            <li><a href="#">Recruiter</a></li>
+            <li><a href="#">Applicant</a></li>
+          </ul>
+        </div>
+        <div className="cls link"><a href="/login">LOGIN</a></div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
